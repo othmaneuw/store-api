9815409435a544a166c370c3ed30a6be9fa96f67
othmaneuw <strangeothmane@gmail.com>
2022-12-25 22:19:48 +0100

    filter by queries v1

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,14 @@
+const Product = require('../models/product');
 
-
-const getAllProducts = (req,res)=>{
+const getAllProducts = async (req,res)=>{
     //throw new Error('error testing ...');
-    res.send('All products');
+    const {featured} = req.query;
+    const queryObject = {};
+    if(featured){
+        queryObject.featured = featured === "true" ? true : false;
+    }
+    const products = await Product.find(queryObject);
+    res.status(200).json({products , nbHits : products.length});
 }
 
 const getAllProductsStatic = (req,res)=>{

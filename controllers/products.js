71ba31a988cc74b1bb944c1ab992f4eba89fc0d8
othmaneuw <strangeothmane@gmail.com>
2022-12-26 09:49:01 +0100

    filter by regex

--- controllers/products.js
+++ controllers/products.js
@@ -2,10 +2,17 @@ const Product = require('../models/product');
 
 const getAllProducts = async (req,res)=>{
     //throw new Error('error testing ...');
-    const {featured} = req.query;
+    console.log(req.query);
+    const {featured,company,name} = req.query;
     const queryObject = {};
     if(featured){
         queryObject.featured = featured === "true" ? true : false;
+    }
+    if(company){
+        queryObject.company = company;
+    }
+    if(name){
+        queryObject.name = {$regex : name , $options : 'i'};
     }
     const products = await Product.find(queryObject);
     res.status(200).json({products , nbHits : products.length});
